37070c422a03e8b44c23149444fe8eb3a9b56144
apollotsantos <hi@tsantos.co>
2025-02-03 20:32:12 +0000

    feat: add content column to WhatsappMessages model and corresponding migration

--- people_api/database/models/models.py
+++ people_api/database/models/models.py
@@ -17,6 +17,7 @@ from sqlmodel import (
     Session,
     SQLModel,
     String,
+    Text,
     UniqueConstraint,
     func,
     select,
@@ -369,6 +370,7 @@ class WhatsappMessages(SQLModel, table=True):
     phone: str = Field(max_length=20, min_length=10)
     message_type: str = Field(max_length=50)
     device_type: str = Field(max_length=50)
+    content: str = Field(sa_column=Column(Text, nullable=False))
 
 
 class IAMRoles(SQLModel, table=True):
