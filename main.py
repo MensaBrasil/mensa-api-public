53028606cf879b736b2f93c75040fb08529c55d1
apollotsantos <hi@tsantos.co>
2025-02-09 14:49:52 +0000

    Feat: Increase uvicorn workers to improve application performance

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from people_api import app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000, workers=4)
